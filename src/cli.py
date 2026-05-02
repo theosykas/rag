c9aa523dd4d7a7bdbc8993c0587a1cfc49c9672f
theosykas <theosykas@icloud.com>
2026-05-02 20:04:38 +0200

    output

--- src/cli.py
+++ src/cli.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel
+from json import JSONDecodeError
 from typing import List, Dict, Any
+from .data_models import RagDataset
+from colorama import Fore
+import json
 
 
 class RagRes(BaseModel):
@@ -16,8 +20,15 @@ class RagCli(BaseModel):
     def search(self, single_qwery: str) -> List[str]:
         pass
 
-    def search_dataset(self, search_res: str) -> List[List[str]]:
-        pass
+    def search_dataset(self, search_path: str, k: int, saving_output:
+                       str = "output") -> None:
+        try:
+            with open(search_path, 'r', encoding='utf-8') as f:
+                dataset = json.load(f)
+            dataset = RagDataset(**dataset)
+        except JSONDecodeError as e:
+            print(f'{Fore.RED}[Error] {e}')
+        return None
 
     def awnser(self, response: str) -> str:
         # prend question return final reponse
--- src/main.py
+++ src/main.py
@@ -1,16 +1,18 @@
 from .generator import Generator
+from colorama import Fore
 from .cli import RagCli
 import fire
+# import sys
 
 
 def main():
-    rag_cli = RagCli()
-    generate = Generator("output")
+    generate = Generator("output.json")
     try:
+        rag_cli = RagCli()
         generate.generate_json(list(range(50)))
         fire.Fire(rag_cli)
     except Exception as e:
-        print(f'error {e}')
+        print(f'{Fore.RED}[ERROR] {e}')
 
 
 if __name__ == "__main__":
